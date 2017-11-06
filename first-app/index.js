8fabcc021d55ab85e327fc4ada60b0ef798a25e2
Bryan <slaters@gmail.com>
2017-11-06 14:15:15 -0500

    Started Eggheads Electron Beginner course

---
+++ first-app/index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { setMainMenu } = require('./main-menu.js');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show : false
+    });
+    mainWindow.loadURL(path.join('file://', __dirname,'index.html'));
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    setMainMenu()
+})
